b2622c1c601738e262e38838b51c613b2167a7e0
Zejdi1 <zl51041@utb-uni.net>
2023-09-28 10:52:09 +0200

    Create Search Todo

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.hmtl",
+  "./src/**/*.{html,ts,jsx,js,tsx}"],
+  theme: {
+    extend: {},
+  
+  plugins: [],
+}
+
+}
